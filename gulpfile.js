18db4c70f22752a55bec73e883f58f6b065abd3e
Otto Rask <ojrask@gmail.com>
2018-07-21 12:59:38 +0300

    Add PostCSS autoprefixing and browsersync
    Helps with some browser compatibility and allows devs
    to locally serve the application with hot-reload and so
    on.

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,24 @@
 
  let gulp = require('gulp');
  let sass = require('gulp-sass');
+ let postcss = require('gulp-postcss');
+ let autoprefixer = require('autoprefixer');
  let clean_css = require('gulp-clean-css');
  let imagemin = require('gulp-imagemin');
+ let browsersync = require('browser-sync').create();
 
  gulp.task('sass', function ()
 {
     return gulp.src('src/sass/**/*.scss')
         .pipe(sass())
         .pipe(clean_css())
+        .pipe(postcss([autoprefixer()]))
         .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('images', function ()
 {
-    return gulp.src('src/img/**/*.{jpeg,jpg,png,gif}')
+    return gulp.src('src/img/**/*.{svg,jpeg,jpg,png,gif}')
         .pipe(imagemin())
         .pipe(gulp.dest('public/img'));
 });
@@ -29,5 +33,18 @@ gulp.task('html', function ()
 });
 
 gulp.task('compile', ['sass', 'images', 'html']);
+
+gulp.task('serve', ['compile'], function ()
+{
+    browsersync.init({
+        server: "./public"
+    });
+
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/img/**/*.{svg,jpeg,jpg,png,gif}', ['images']);
+    gulp.watch('src/html/**/*.html', ['html']);
+
+    gulp.watch('src/html/**/*.html').on('change', browsersync.reload);
+});
 
 gulp.task('default', ['compile']);
